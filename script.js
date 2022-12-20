6e2be5db8f0187437db7ad20c71ec3c177734dd0
CK151 <andersonoki15@gmail.com>
2022-12-20 14:09:53 +0300

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+const fab = document.getElementById("fab");
+const fabBtns = document.querySelector(".fab-btns");
+
+
+fab.addEventListener("click", () => {
+    fabBtns.classList.toggle("show");
+});
